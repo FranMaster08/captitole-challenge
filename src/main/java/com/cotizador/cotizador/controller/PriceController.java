0c52c0a7c11aa5b874dbded93dcc8f1af09bdaab
Jimenez Cohen, Francisco Javier (Externos - AcidLabs) <franciscojavier.jimenezcohen@externos-cl.cencosud.com>
2023-12-20 12:56:32 -0500

    add: se agrega endpoint de obtener todos los precios

--- src/main/java/com/cotizador/cotizador/controller/PriceController.java
+++ src/main/java/com/cotizador/cotizador/controller/PriceController.java
@@ -1,10 +1,33 @@
 package com.cotizador.cotizador.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cotizador.cotizador.entities.Prices;
+import com.cotizador.cotizador.service.PriceService;
 
 @RestController
 @RequestMapping("/prices")
 public class PriceController {
+    private final PriceService priceService;
+
+    public PriceController(PriceService priceService) {
+        this.priceService = priceService;
+    }
+
+
+    /**
+     * Obtiene una lista de todos los precios.
+     *
+     * @return ResponseEntity con la lista de precios.
+     */
+    @GetMapping("/get-prices")
+    public ResponseEntity<List<Prices>> getPrices() {
+        return priceService.getPrices();
+    }
     
 }
