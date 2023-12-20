5b8a4d3e8570f6cee8ac045a67f01449a21dce8a
Jimenez Cohen, Francisco Javier (Externos - AcidLabs) <franciscojavier.jimenezcohen@externos-cl.cencosud.com>
2023-12-20 12:51:35 -0500

    add : se adecua servicio y se injecta repositorio

--- src/main/java/com/cotizador/cotizador/service/PriceService.java
+++ src/main/java/com/cotizador/cotizador/service/PriceService.java
@@ -1,14 +1,24 @@
 package com.cotizador.cotizador.service;
 
+import com.cotizador.cotizador.entities.Prices;
+import com.cotizador.cotizador.repositories.PricesRepository;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PriceService {
+    private final PricesRepository pricesRepository;
 
+    public PriceService(PricesRepository priceRepository) {
+        this.pricesRepository = priceRepository;
+    }
 
-
-
-
-
+    public ResponseEntity<List<Prices>> getPrices() {
+        List<Prices> priceList = pricesRepository.findAll();
+        return ResponseEntity.ok(priceList);
+    }
 
 }
